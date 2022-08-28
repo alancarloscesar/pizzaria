eb1697f3f34e81128a21cefe5cfb433f6c0de98a
alancesar <alancarloscesar@gmail.com>
2022-08-28 16:00:29 -0300

    iniciando estrutura do backend e criando servidor

---
+++ backend/src/routes.ts
@@ -0,0 +1,11 @@
+import {Router, Response, Request} from 'express'
+
+const router = Router();
+
+router.get("/teste", (req:Request, res:Response)=>{
+    return res.json({
+        ok: true
+    })
+})
+
+export {router}
