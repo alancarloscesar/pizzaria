3e3bfd8e38390902e0494787b22990ccf97ae381
alancesar <alancarloscesar@gmail.com>
2022-09-19 18:43:38 -0300

    Check para verificar se o cliente vai pedir dois sabores de um pizza por exemplo

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from 'react-native'
 
 import { useRoute, RouteProp } from '@react-navigation/native'//importa para pegar os dados que estão no navigate
 import { Ionicons } from '@expo/vector-icons'
+
 
 
 type RouteDetailParams = {//tipagem dos dados recebidos
@@ -16,7 +17,9 @@ type OrderTypeProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
 
+    const [check, setCheck] = useState(false)
     const route = useRoute<OrderTypeProps>()
+
 
     return (
         <SafeAreaView style={styles.container}>
@@ -31,9 +34,35 @@ export default function Order() {
                 <Text style={styles.textInput}>Pizzas</Text>
             </TouchableOpacity>
 
+            <View style={styles.areaCheckTam}>
+                <TouchableOpacity style={styles.check}>
+
+                    <TouchableOpacity onPress={() => setCheck(!check)}>
+                        {
+                            check ?
+                                <Ionicons name='checkbox-outline' size={25} color='#fff' />
+                                :
+                                <View style={styles.notCheck}></View>
+                        }
+                    </TouchableOpacity>
+
+                    <Text style={{ fontSize: 17, color: '#fff', marginLeft: 10 }}>2 sabores</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.input, { width: '60%', alignItems: 'center' }]}>
+                    <Text style={styles.textInput}>M</Text>
+                </TouchableOpacity>
+            </View>
+
+
             <TouchableOpacity style={styles.input}>
-                <Text style={styles.textInput}>Pizza de calabresa</Text>
+                <Text style={styles.textInput}>Primeiro sabor</Text>
             </TouchableOpacity>
+
+            {check && (
+                <TouchableOpacity style={styles.input}>
+                    <Text style={styles.textInput}>Segundo sabor</Text>
+                </TouchableOpacity>
+            )}
 
             <View style={styles.areaQtd}>
                 <Text style={styles.qtdText}>Quantidade: </Text>
@@ -67,7 +96,9 @@ const styles = StyleSheet.create({
     tabelArea: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginBottom: '8%'
+        marginBottom: '4%',
+        justifyContent:'flex-end'
+
     },
     tableNumber: {
         color: '#fff',
@@ -135,6 +166,22 @@ const styles = StyleSheet.create({
         color: '#101026',
         fontSize: 20,
         fontWeight: '500'
+    },
+    areaCheckTam: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    check: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 45,
+    },
+    notCheck: {
+        width: 20,
+        height: 20,
+        borderColor: '#fff',
+        borderWidth: 1
     }
 
 })
